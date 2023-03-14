5da98cbb59892afed058c18bb7d70d18b0af5252
Ngô Khánh Nguyên <khanhnguyen.vlu@gmail.com>
2023-03-14 21:47:04 +0700

    using web socket to display data real time

--- server.js
+++ server.js
@@ -1,37 +1,60 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
+const WebSocket = require('ws');
+const path = require('path');
 
-async function crawler() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  
-  await page.goto('https://csgoempire.com/');
+const app = express();
+const wss = new WebSocket.Server({ port: 8080 });
 
-  const divContainingClass = await page.waitForSelector('.previous-rolls-enter-active');
-  const divContent = await page.evaluate(div => div.innerHTML, divContainingClass);
-  
-  const now = new Date();
-  const timestamp = now.toLocaleString();
+app.use(express.static(path.join(__dirname, 'public')));
 
-  if (divContent.includes('coin-t')) {
-    console.log(`${timestamp} - T`);
-  } else if (divContent.includes('coin-ct')) {
-    console.log(`${timestamp} - CT`);
-  } else if (divContent.includes('coin-bonus')) {
-    console.log(`${timestamp} - Bonus`);
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  async function crawler() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://csgoempire.com/');
+    const divContainingClass = await page.waitForSelector('.previous-rolls-enter-active');
+    const divContent = await page.evaluate(div => div.innerHTML, divContainingClass);
+    const now = new Date();
+    const timestamp = now.toLocaleString();
+
+    if (divContent.includes('coin-t')) {
+      const message = `${timestamp} - T`;
+      console.log(message);
+      ws.send(message);
+    } else if (divContent.includes('coin-ct')) {
+      const message = `${timestamp} - CT`;
+      console.log(message);
+      ws.send(message);
+    } else if (divContent.includes('coin-bonus')) {
+      const message = `${timestamp} - Bonus`;
+      console.log(message);
+      ws.send(message);
+    }
+
+    await browser.close();
   }
 
-  await browser.close();
-}
-
-async function loop() {
-  while (true) {
-    await crawler();
-    await delay(3000); // delay for 3 seconds before next iteration
+  async function loop() {
+    while (true) {
+      await crawler();
+      await delay(3000); // delay for 3 seconds before next iteration
+    }
   }
-}
 
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
-loop();
+  loop();
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
