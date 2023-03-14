34496e1f840ea15e27462e6bd03b1d1db9875247
Ngô Khánh Nguyên <khanhnguyen.vlu@gmail.com>
2023-03-14 22:22:33 +0700

    using socket io for display data real time

--- server.js
+++ server.js
@@ -1,58 +1,57 @@
 const puppeteer = require('puppeteer');
-const express = require('express');
-const WebSocket = require('ws');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-const app = express();
-const wss = new WebSocket.Server({ port: 8080 });
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
+async function crawler() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  await page.goto('https://csgoempire.com/');
 
-  async function crawler() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://csgoempire.com/');
-    const divContainingClass = await page.waitForSelector('.previous-rolls-enter-active');
-    const divContent = await page.evaluate(div => div.innerHTML, divContainingClass);
-    const now = new Date();
-    const timestamp = now.toLocaleString();
+  const divContainingClass = await page.waitForSelector('.previous-rolls-enter-active');
+  const divContent = await page.evaluate(div => div.innerHTML, divContainingClass);
+  
+  const now = new Date();
+  const timestamp = now.toLocaleString();
 
-    if (divContent.includes('coin-t')) {
-      const message = `${timestamp} - T`;
-      console.log(message);
-      ws.send(message);
-    } else if (divContent.includes('coin-ct')) {
-      const message = `${timestamp} - CT`;
-      console.log(message);
-      ws.send(message);
-    } else if (divContent.includes('coin-bonus')) {
-      const message = `${timestamp} - Bonus`;
-      console.log(message);
-      ws.send(message);
-    }
-
-    await browser.close();
+  if (divContent.includes('coin-t')) {
+    console.log(timestamp, 'coin-t');
+    io.emit('log', `${timestamp} - T`);
+  } else if (divContent.includes('coin-ct')) {
+    console.log(timestamp, 'coin-ct');
+    io.emit('log', `${timestamp} - CT`);
+  } else if (divContent.includes('coin-bonus')) {
+    console.log(timestamp, 'coin-bonus');
+    io.emit('log', `${timestamp} - Bonus`);
   }
 
-  async function loop() {
-    while (true) {
-      await crawler();
-      await delay(3000); // delay for 3 seconds before next iteration
-    }
-  }
+  await browser.close();
+}
 
-  function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+async function loop() {
+  while (true) {
+    await crawler();
+    await delay(3000); // delay for 3 seconds before next iteration
   }
+}
 
-  loop();
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+loop();
+
+io.on('connection', (socket) => {
+  console.log('client connected');
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+http.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
