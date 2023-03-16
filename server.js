eeaa2b8ec8186facbc1f8e066a514a0b97357afe
Ngô Khánh Nguyên <khanhnguyen.vlu@gmail.com>
2023-03-16 22:08:03 +0700

    update ui

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const path = require('path');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  fs.readFile(__dirname + '/data/logs.json', 'utf8', (err, data) => {
+    if (err) throw err;
+
+    // Parse the JSON data into an array of objects
+    const logs = JSON.parse(data);
+
+    // Get 120 latest objects
+    const latestLogs = logs.slice(0, 120);
+
+    res.render("index", {logs: latestLogs});
+  })
 });
 
 async function crawler() {
@@ -29,29 +41,35 @@ async function crawler() {
   const divContent = await page.evaluate(div => div.innerHTML, divContainingClass);
 
   const now = new Date();
-  const timestamp = now.toLocaleString();
+  const timestamp = now.toLocaleString().replace(', ', '@');
 
   let msgType = '';
 
   if (divContent.includes('alt="t"')) {
     console.log(timestamp, 'coin-t');
-    io.emit('log', `T`);
-    msgType = 'T';
+    io.emit('log', `coin-t`);
+    msgType = 'coin-t';
   } else if (divContent.includes('alt="ct"')) {
     console.log(timestamp, 'coin-ct');
-    io.emit('log', `CT`);
-    msgType = 'CT';
+    io.emit('log', `coin-ct`);
+    msgType = 'coin-ct';
   } else if (divContent.includes('alt="bonus"')) {
     console.log(timestamp, 'coin-bonus');
-    io.emit('log', `Bonus`);
-    msgType = 'Bonus';
+    io.emit('log', `coin-bonus`);
+    msgType = 'coin-bonus';
   }
 
   // Write the log to file
-  const logLine = `${timestamp},${msgType}\n`;
-  fs.appendFile(__dirname + '/data/logs.txt', logLine, (err) => {
-    if (err) throw err;
-  });
+  const logsFilePath = __dirname + '/data/logs.json';
+
+  try {
+    const logsContent = await fs.promises.readFile(logsFilePath, 'utf8');
+    const logs = JSON.parse(logsContent);
+    logs.unshift({ timestamp, coin: msgType });
+    await fs.promises.writeFile(logsFilePath, JSON.stringify(logs));
+  } catch (err) {
+    console.error('Error writing log to file:', err);
+  }
 
   await browser.close();
 }
